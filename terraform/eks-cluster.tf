a9679263762fd523ab6965bd6e14c8d22d57f9c5
mohitjain1911 <mohitjain19113@gmail.com>
2024-02-09 19:46:49 +0530

    Changed

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -9,7 +9,7 @@ module "eks" {
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
-  eks_managed_node_group_defaults = {
+  eks_managed_node_group_defaults =   {
     ami_type = "AL2_x86_64"
 
   }
